c178c165b1e28877debbf3b8a596e33f7da5b8aa
2life <jiawen14th@gmail.com>
2020-05-27 00:12:11 +0800

    demo004.gif

--- demo003/昼夜切换/demo.js
+++
@@ -1,7 +0,0 @@
-$("body").click(function() {
-    $("body").toggleClass("dark-mode");
-
-    var angle = $('.dark-mode-planet').data('angle') + 360 || 360;
-    $('.dark-mode-planet').css({'transform': 'rotate(' + angle + 'deg'});
-    $('.dark-mode-planet').data('angle',angle);
-});
